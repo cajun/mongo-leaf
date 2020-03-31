4bbe452ffb609b75a3fa470610b11f9501897cd3
Cajun <zac@kleinpeter.org>
2020-03-31 05:12:12 -0500

    Updating Static builds of mongo-c-sys

--- mongo-c-sys/build.rs
+++ mongo-c-sys/build.rs
@@ -129,8 +129,7 @@ fn win(_mongoc_version: &str) {
 }
 
 fn main() {
-    //let mongoc_version = "1.15.2";
-    let mongoc_version = env!("CARGO_PKG_VERSION");
+    let mongoc_version = "1.16.2";
 
     #[cfg(target_env = "msvc")]
     win(mongoc_version);
